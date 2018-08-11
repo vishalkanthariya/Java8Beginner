29007e84b006db1d1124fed8c66d775035a19cb4
vishalkanthariya <42299066+vishalkanthariya@users.noreply.github.com>
2018-08-11 21:35:26 +0530

    changes done

--- NewJavaPractice/src/com/newjavapractice/NewHello.java
+++ NewJavaPractice/src/com/newjavapractice/NewHello.java
@@ -5,7 +5,7 @@ public class NewHello {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("hello India");
-		
+		System.out.println("hello 403");
 	}
 
 }
