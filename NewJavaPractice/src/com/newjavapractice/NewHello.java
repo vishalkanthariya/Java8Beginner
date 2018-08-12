f221de40377a03a8ab37646bd51d8a728c3be647
vishalkanthariya <vishalkanthariya>
2018-08-12 12:51:46 +0530

    gff

--- NewJavaPractice/src/com/newjavapractice/NewHello.java
+++ NewJavaPractice/src/com/newjavapractice/NewHello.java
@@ -5,11 +5,7 @@ public class NewHello {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("hello India");
-<<<<<<< HEAD
 		System.out.println("jai shri ram");
-=======
-		System.out.println("hello 403");
->>>>>>> 29007e84b006db1d1124fed8c66d775035a19cb4
 	}
 
 }
