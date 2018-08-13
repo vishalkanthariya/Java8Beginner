0167721f7072cf5a16c3fee18212d0b954e181ae
vishalkanthariya <vishalkanthariya>
2018-08-13 21:11:16 +0530

    code changes

--- NewJavaPractice/src/com/newjavapractice/TestLemda.java
+++ NewJavaPractice/src/com/newjavapractice/TestLemda.java
@@ -1,10 +1,20 @@
 package com.newjavapractice;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class TestLemda {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		List<Integer> numbers=Arrays.asList(1,2,3,4,5);
+       //   numbers.forEach((Integer value)->System.out.println(value));
+		System.out.println(
+				numbers.stream()
+				.filter(e->e%2==0)
+				.mapToInt(e->e*2)
+				.sum()
+				);
 	}
 
 }
